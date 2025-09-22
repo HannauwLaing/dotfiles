f8529c68a9878c81241644996b43f92689689dac
Hannauw Laing <27086534@sun.ac.za>
2025-09-22 14:24:21 +0200

    Forgot to add variable value

--- .setup_files/setup_zsh.sh
+++ .setup_files/setup_zsh.sh
@@ -3,6 +3,7 @@
 SRC=$(pwd)
 
 #currently only doing fonts
+FONT_DL_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip"
 FONT_NAME_DISPLAY="FiraCode Nerd Font Mono"
 FONT_POINT_SIZE="12"
 
